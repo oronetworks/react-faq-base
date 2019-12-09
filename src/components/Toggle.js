0dad186e3e10567fcb5caf14b6c518f611839f98
shreyas1995 <shreyas.kanzarkar@easebuzz.in>
2019-12-10 01:57:26 +0530

    toggle animation

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,18 +1,49 @@
-import React from "react";
-import { Consumer } from "../context";
-
+import React, {useState, useEffect} from "react";
+import {Consumer} from "../context";
+import '../assets/toggle.scss'
 export default props => {
-  let { data } = props;
+  let {data} = props;
   let count = data.count;
+  let jumpTillSec = 10000;
+  let startJumpInSec = 15000;
+  let highestTimeoutId = setTimeout(";");
+  const [isAnimate, setIsAnimate] = useState(true)
+
+  useEffect(() => {
+    jumpTillSec = 15000;
+    startJumpInSec = 15000;
+    if (count > 0) {
+      setIsAnimate(true);
+    }
+    for (let i = 0; i < highestTimeoutId; i++) {
+      clearTimeout(i);
+    }
+    startJump();
+  }, [count]);
+
+  let startJump = () => {
+    if (count === 0) {
+      return;
+    }
+    setTimeout(() => {
+      setIsAnimate(false);
+      setTimeout(() => {
+        setIsAnimate(true);
+        startJumpInSec *= 2;
+        startJump();
+      }, startJumpInSec);
+    }, jumpTillSec)
+  }
+
   return (
     <Consumer>
-      {({ primary_color, bot_inverted, windowTrigger }) => (
+      {({primary_color, bot_inverted, windowTrigger}) => (
         <div
-          className="toggle"
+          className={`${count > 0 && isAnimate ? 'box bounce toggle' : 'toggle'}`}
           onClick={windowTrigger}
-          style={{ backgroundColor: primary_color }}
+          style={{backgroundColor: primary_color}}
         >
-          <img src={bot_inverted} width={33} height={33} />
+          <img src={bot_inverted} width={33} height={33}/>
           {!!count && (
             <p
               className="count"
